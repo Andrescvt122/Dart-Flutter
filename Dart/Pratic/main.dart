ce52ee4ab96587f3b21b08cd232a2a70f9b4b91d
Andrescvt122 <{{your_github_email}}>
2025-08-06 20:53:07 -0500

    "Initial-commit"

---
+++ Dart/Pratic/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Practica de asserts
+  //   int value = 5;
+  //   assert(value != 10, print('El valor debe ser mayor que cero'));
+  //   print('El valor es $value');
+  // Practica de recrods
+  ({int id, String name}) employee = (id: 1, name: 'John Doe');
+  print('Employee ID: ${employee.id}, Name: ${employee.name}');
+}
